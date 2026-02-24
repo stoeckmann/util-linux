29f529a6eafdd27c3329ccb3040f0310387e4270
Tobias Stoeckmann <tobias@stoeckmann.org>
2026-02-24 22:14:53 +0100

    wip

--- lib/fileeq.c
+++ lib/fileeq.c
@@ -263,7 +263,7 @@ size_t ul_fileeq_set_size(struct ul_fileeq *eq, uint64_t filesiz,
 	switch (eq->method->id) {
 	case UL_FILEEQ_MEMCMP:
 		/* align file size */
-		filesiz = (filesiz + readsiz) / readsiz * readsiz;
+		filesiz = (filesiz + readsiz - 1) / readsiz * readsiz;
 		break;
 	default:
 		digsiz = eq->method->digsiz;
@@ -477,6 +477,10 @@ static ssize_t get_intro(struct ul_fileeq *eq, struct ul_fileeq_data *data,
 		DBG(DATA, ul_debugobj(data, " read %zd bytes [%zu wanted] intro", rsz, sizeof(data->intro)));
 		if (rsz < 0)
 			return -1;
+		if (rsz == 0 || (uint64_t) rsz >= eq->filesiz) {
+			data->is_eof = 1;
+			ul_fileeq_data_close_file(data);
+		}
 		data->nblocks = 1;
 	}
 
